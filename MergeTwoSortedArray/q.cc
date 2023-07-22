acb548077cefafb034aa7658e13ac091a93b8944
Anshumali Karna <anshumali.karna99@gmail.com>
2023-07-22 15:45:34 +0530

    first commit

---
+++ MergeTwoSortedArray/q.cc
@@ -0,0 +1,45 @@
+#import <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* cur = dummy;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                cur->next = list1;
+                list1 = list1->next;
+            } else {
+                cur->next = list2;
+                list2 = list2->next;
+            }
+            cur = cur->next;
+        }
+        if (list1) cur->next = list1;
+        if (list2) cur->next = list2;
+        return dummy->next;
+    }
+};
+
+int main(int argc, const char** argv) {
+    Solution s;
+    ListNode* list1 = new ListNode(1);
+    list1->next = new ListNode(2);
+    list1->next->next = new ListNode(4);
+    ListNode* list2 = new ListNode(1);
+    list2->next = new ListNode(3);
+    list2->next->next = new ListNode(4);
+    ListNode* res = s.mergeTwoLists(list1, list2);
+    while (res) {
+        cout << res->val << " ";
+        res = res->next;
+    }
+    return 0;
+}
